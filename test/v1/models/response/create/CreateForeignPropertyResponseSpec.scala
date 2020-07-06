d4c0b22874b49893ff09f69d0d6d9d0fce601bb9
“Thepworth95” <56401451+Thepworth95@users.noreply.github.com>
2020-07-06 14:47:10 +0100

    Response model and spec

---
+++ test/v1/models/response/create/CreateForeignPropertyResponseSpec.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.response.create
+
+import play.api.libs.json.Json
+import support.UnitSpec
+import v1.models.utils.JsonErrorValidators
+
+class CreateForeignPropertyResponseSpec extends UnitSpec with JsonErrorValidators {
+
+  val createForeignPropertyResponse = CreateForeignPropertyResponse(
+    "4557ecb5-fd32-48cc-81f5-e6acd1099f3c"
+  )
+
+  val json = Json.parse(
+    """{
+      |  "submissionId": "4557ecb5-fd32-48cc-81f5-e6acd1099f3c"
+      |}""".stripMargin)
+
+  "reads" when {
+    "passed a valid JSON" should {
+      "return a valid model" in {
+        json.as[CreateForeignPropertyResponse] shouldBe createForeignPropertyResponse
+      }
+    }
+  }
+  "writes" when {
+    "passed valid model" should {
+      "return valid JSON" in {
+        Json.toJson(CreateForeignPropertyResponse) shouldBe json
+      }
+    }
+  }
+}

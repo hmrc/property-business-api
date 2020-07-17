984ba9b8a3d40c76b4384ab8e4696f7fb8959753
“DreamcastKnight” <56401347+DreamcastKnight@users.noreply.github.com>
2020-07-17 10:21:31 +0100

    service done

---
+++ app/v1/services/AmendForeignPropertyAnnualSubmissionService.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import cats.implicits._
+import cats.data.EitherT
+import javax.inject.{Inject, Singleton}
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.Logging
+import v1.connectors.AmendForeignPropertyAnnualSubmissionConnector
+import v1.controllers.EndpointLogContext
+import v1.models.errors._
+import v1.models.request.amendForeignPropertyAnnualSubmission.AmendForeignPropertyAnnualSubmissionRequest
+import v1.support.DesResponseMappingSupport
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class AmendForeignPropertyAnnualSubmissionService @Inject()
+(connector: AmendForeignPropertyAnnualSubmissionConnector) extends DesResponseMappingSupport with Logging {
+
+  def amendForeignProperty(request: AmendForeignPropertyAnnualSubmissionRequest)(
+    implicit hc: HeaderCarrier,
+    ec: ExecutionContext,
+    logContext: EndpointLogContext): Future[AmendForeignPropertyPeriodSummaryServiceOutcome] = {
+
+    val result = for {
+      desResponseWrapper <- EitherT(connector.amendForeignPropertyAnnualSubmission(request)).leftMap(mapDesErrors(desErrorMap))
+    } yield desResponseWrapper
+
+    result.value
+  }
+
+  private def desErrorMap =
+    Map(
+      "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
+      "INVALID_BUSINESS_ID" -> BusinessIdFormatError,
+      "NOT_FOUND" -> NotFoundError,
+      "SERVER_ERROR" -> DownstreamError,
+      "SERVICE_UNAVAILABLE" -> DownstreamError
+    )
+}
---
+++ test/v1/services/AmendForeignPropertyAnnualSubmissionServiceSpec.scala
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import support.UnitSpec
+import uk.gov.hmrc.domain.Nino
+import uk.gov.hmrc.http.HeaderCarrier
+import v1.controllers.EndpointLogContext
+import v1.mocks.connectors.MockAmendForeignPropertyAnnualSubmissionConnector
+import v1.models.errors._
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.amendForeignPropertyAnnualSubmission.{AmendForeignPropertyAnnualSubmissionRequest, AmendForeignPropertyAnnualSubmissionRequestBody}
+import v1.models.request.amendForeignPropertyAnnualSubmission.foreignFhlEea.{ForeignFhlEea, ForeignFhlEeaAdjustments, ForeignFhlEeaAllowances}
+import v1.models.request.amendForeignPropertyAnnualSubmission.foreignProperty.{ForeignPropertyAdjustments, ForeignPropertyAllowances, ForeignPropertyEntry}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class AmendForeignPropertyAnnualSubmissionServiceSpec extends UnitSpec {
+
+  val nino = Nino("AA123456A")
+  val businessId = "XAIS12345678910"
+  val taxYear = "2020-21"
+  private val correlationId = "X-123"
+
+  private val foreignFhlEea = ForeignFhlEea(
+    Some(ForeignFhlEeaAdjustments(
+      Some(5000.99),
+      Some(5000.99),
+      Some(true)
+    )),
+    Some(ForeignFhlEeaAllowances(
+      Some(5000.99),
+      Some(5000.99),
+      Some(5000.99),
+      Some(5000.99)
+    ))
+  )
+
+  private val foreignPropertyEntry = ForeignPropertyEntry(
+    "FRA",
+    Some(ForeignPropertyAdjustments(
+      Some(5000.99),
+      Some(5000.99)
+    )),
+    Some(ForeignPropertyAllowances(
+      Some(5000.99),
+      Some(5000.99),
+      Some(5000.99),
+      Some(5000.99),
+      Some(5000.99),
+      Some(5000.99),
+      Some(5000.99)
+    ))
+  )
+
+  val body = AmendForeignPropertyAnnualSubmissionRequestBody(
+    Some(foreignFhlEea),
+    Some(Seq(foreignPropertyEntry))
+  )
+
+  private val request = AmendForeignPropertyAnnualSubmissionRequest(nino, businessId, taxYear, body)
+
+  trait Test extends MockAmendForeignPropertyAnnualSubmissionConnector {
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+    implicit val logContext: EndpointLogContext = EndpointLogContext("c", "ep")
+
+    val service = new AmendForeignPropertyAnnualSubmissionService(
+      connector = mockAmendForeignPropertyAnnualSubmissionConnector
+    )
+  }
+
+  "service" should {
+    "service call successful" when {
+      "return mapped result" in new Test {
+        MockAmendForeignPropertyAnnualSubmissionConnector.amendForeignProperty(request)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, ()))))
+
+        await(service.amendForeignProperty(request)) shouldBe Right(ResponseWrapper(correlationId, ()))
+      }
+    }
+  }
+
+  "unsuccessful" should {
+    "map errors according to spec" when {
+
+      def serviceError(desErrorCode: String, error: MtdError): Unit =
+        s"a $desErrorCode error is returned from the service" in new Test {
+
+          MockAmendForeignPropertyAnnualSubmissionConnector.amendForeignProperty(request)
+            .returns(Future.successful(Left(ResponseWrapper(correlationId, DesErrors.single(DesErrorCode(desErrorCode))))))
+
+          await(service.amendForeignProperty(request)) shouldBe Left(ErrorWrapper(Some(correlationId), error))
+        }
+
+      val input = Seq(
+        "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
+        "INVALID_BUSINESS_ID" -> BusinessIdFormatError,
+        "NOT_FOUND" -> NotFoundError,
+        "SERVER_ERROR" -> DownstreamError,
+        "SERVICE_UNAVAILABLE" -> DownstreamError
+      )
+
+      input.foreach(args => (serviceError _).tupled(args))
+    }
+  }
+
+
+}

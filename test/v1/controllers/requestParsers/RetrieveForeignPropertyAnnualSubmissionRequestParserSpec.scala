d92592a47335ebf14f24a487f69e0f740ac3755f
“Thepworth95” <56401451+Thepworth95@users.noreply.github.com>
2020-07-20 15:43:32 +0100

    Retrieve parser and validator

---
+++ test/v1/controllers/requestParsers/RetrieveForeignPropertyAnnualSubmissionRequestParserSpec.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers
+
+import support.UnitSpec
+import uk.gov.hmrc.domain.Nino
+import v1.mocks.validators.MockRetrieveForeignPropertyAnnualSubmissionValidator
+import v1.models.errors.{BadRequestError, BusinessIdFormatError, ErrorWrapper, NinoFormatError}
+import v1.models.request.retrieveForeignPropertyAnnualSubmission._
+
+class RetrieveForeignPropertyAnnualSubmissionRequestParserSpec extends UnitSpec {
+  val nino = "AA123456B"
+  val businessId = "XAIS12345678901"
+  val taxYear = "2021-22"
+
+  val inputData =
+    RetrieveForeignPropertyAnnualSubmissionRawData(nino, businessId, taxYear)
+
+  trait Test extends MockRetrieveForeignPropertyAnnualSubmissionValidator {
+    lazy val parser = new RetrieveForeignPropertyAnnualSubmissionRequestParser(mockValidator)
+  }
+
+  "parse" should {
+    "return a request object" when {
+      "valid request data is supplied" in new Test {
+        MockRetrieveForeignPropertyAnnualSubmissionValidator.validate(inputData).returns(Nil)
+
+        parser.parseRequest(inputData) shouldBe Right(RetrieveForeignPropertyAnnualSubmissionRequest(Nino(nino), businessId, taxYear))
+      }
+    }
+    "return an ErrorWrapper" when {
+      "a single validation error occurs" in new Test {
+        MockRetrieveForeignPropertyAnnualSubmissionValidator.validate(inputData)
+          .returns(List(NinoFormatError))
+
+        parser.parseRequest(inputData) shouldBe
+          Left(ErrorWrapper(None, NinoFormatError, None))
+      }
+      "multiple validation errors occur" in new Test {
+        MockRetrieveForeignPropertyAnnualSubmissionValidator.validate(inputData)
+          .returns(List(NinoFormatError, BusinessIdFormatError))
+
+        parser.parseRequest(inputData) shouldBe
+          Left(ErrorWrapper(None, BadRequestError, Some(Seq(NinoFormatError, BusinessIdFormatError))))
+      }
+    }
+  }
+}

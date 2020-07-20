8365037bd6d163f2f7e7457f728972c0f2bd5d21
“Thepworth95” <56401451+Thepworth95@users.noreply.github.com>
2020-07-20 10:09:12 +0100

    Delete Annual Submissions Parser and Validator

---
+++ test/v1/controllers/requestParsers/validators/DeleteForeignPropertyAnnualSubmissionValidatorSpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers.validators
+
+import support.UnitSpec
+import v1.models.errors.{BusinessIdFormatError, NinoFormatError, SubmissionIdFormatError}
+import v1.models.request.deleteForeignPropertyAnnualSubmission.DeleteForeignPropertyAnnualSubmissionRawData
+
+class DeleteForeignPropertyAnnualSubmissionValidatorSpec extends UnitSpec {
+
+  private val validNino = "AA123456A"
+  private val validBusinessId = "XAIS12345678901"
+  private val validTaxYear = "2021-22"
+
+  private val validator = new DeleteForeignPropertyAnnualSubmissionValidator
+
+  "running a validation" should {
+    "return no errors" when {
+      "a valid request is supplied" in {
+        validator.validate(DeleteForeignPropertyAnnualSubmissionRawData(validNino, validBusinessId, validTaxYear)) shouldBe Nil
+      }
+    }
+    "return a path parameter format error" when {
+      "an invalid nino is supplied" in {
+        validator.validate(DeleteForeignPropertyAnnualSubmissionRawData("Walrus", validBusinessId, validTaxYear)) shouldBe List(NinoFormatError)
+      }
+      "an invalid businessId is supplied" in {
+        validator.validate(DeleteForeignPropertyAnnualSubmissionRawData(validNino, "Beans", validTaxYear)) shouldBe List(BusinessIdFormatError)
+      }
+      "an invalid taxYear is supplied" in {
+        validator.validate(DeleteForeignPropertyAnnualSubmissionRawData(validNino, validBusinessId, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")) shouldBe List(TaxYearFormatError)
+      }
+      "multiple format errors are made" in {
+        validator.validate(DeleteForeignPropertyAnnualSubmissionRawData("Walrus", "Beans", "ABCDEFGHIJKLMNOPQRSTUVWXYZ")) shouldBe List(NinoFormatError, BusinessIdFormatError, TaxYearFormatError)
+      }
+    }
+  }
+}

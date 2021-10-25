f66fe03d2a09875a8f4e6445475e87584e78be9b
james-work-account <31282758+james-work-account@users.noreply.github.com>
2021-10-25 10:10:56 +0100

    [MTDSA-10092] rename annual summary -> annual submission for retrieve uk prop models

---
+++ app/v2/models/response/retrieveUkPropertyAnnualSubmission/RetrieveUkPropertyAnnualSubmissionResponse.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.models.response.retrieveUkPropertyAnnualSubmission
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+import v2.models.response.retrieveUkPropertyAnnualSubmission.ukFhlProperty.UkFhlProperty
+import v2.models.response.retrieveUkPropertyAnnualSubmission.ukNonFhlProperty.UkNonFhlProperty
+
+case class RetrieveUkPropertyAnnualSubmissionResponse(submittedOn: String,
+                                                      ukFhlProperty: Option[UkFhlProperty],
+                                                      ukNonFhlProperty: Option[UkNonFhlProperty])
+
+object RetrieveUkPropertyAnnualSubmissionResponse {
+  implicit val writes: OWrites[RetrieveUkPropertyAnnualSubmissionResponse] = Json.writes[RetrieveUkPropertyAnnualSubmissionResponse]
+
+  implicit val reads: Reads[RetrieveUkPropertyAnnualSubmissionResponse] = (
+    (__ \ "submittedOn").read[String] and
+      (__ \ "ukFhlProperty").readNullable[UkFhlProperty] and
+      (__ \ "ukOtherProperty").readNullable[UkNonFhlProperty]
+  )(RetrieveUkPropertyAnnualSubmissionResponse.apply _)
+}
---
+++ test/v2/models/response/retrieveUkPropertyAnnualSubmission/ukFhlProperty/UkFhlPropertySpec.scala
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.models.response.retrieveUkPropertyAnnualSubmission.ukFhlProperty
+
+import play.api.libs.json.{JsValue, Json}
+import support.UnitSpec
+
+class UkFhlPropertySpec extends UnitSpec {
+  val downstreamJson: JsValue = Json.parse("""
+      |{
+      |   "allowances":{
+      |      "annualInvestmentAllowance":123.45,
+      |      "businessPremisesRenovationAllowance":345.56,
+      |      "otherCapitalAllowance":345.34,
+      |      "propertyIncomeAllowance":453.45,
+      |      "electricChargePointAllowance":453.34,
+      |      "zeroEmissionsCarAllowance":123.12
+      |   },
+      |   "adjustments":{
+      |      "lossBroughtForward":343.34,
+      |      "privateUseAdjustment":454.45,
+      |      "balancingCharge":231.45,
+      |      "periodOfGraceAdjustment":true,
+      |      "businessPremisesRenovationAllowanceBalancingCharges":567.67,
+      |      "nonResidentLandlord":true,
+      |      "ukFhlRentARoom":{
+      |         "jointlyLet":true
+      |      }
+      |   }
+      |}
+      |""".stripMargin)
+
+  val model: UkFhlProperty = UkFhlProperty(
+    adjustments = Some(
+      UkFhlPropertyAdjustments(
+        lossBroughtForward = Some(343.34),
+        privateUseAdjustment = Some(454.45),
+        balancingCharge = Some(231.45),
+        periodOfGraceAdjustment = true,
+        businessPremisesRenovationAllowanceBalancingCharges = Some(567.67),
+        nonResidentLandlord = true,
+        rentARoom = Some(
+          UkFhlPropertyRentARoom(
+            jointlyLet = true
+          )),
+      )
+    ),
+    allowances = Some(
+      UkFhlPropertyAllowances(
+        Some(123.45),
+        Some(345.56),
+        Some(345.34),
+        Some(453.45),
+        Some(453.34),
+        Some(123.12)
+      )
+    )
+  )
+
+  val mtdJson: JsValue = Json.parse("""
+      |{
+      |   "allowances":{
+      |      "annualInvestmentAllowance":123.45,
+      |      "businessPremisesRenovationAllowance":345.56,
+      |      "otherCapitalAllowance":345.34,
+      |      "propertyIncomeAllowance":453.45,
+      |      "electricChargePointAllowance":453.34,
+      |      "zeroEmissionsCarAllowance":123.12
+      |   },
+      |   "adjustments":{
+      |      "lossBroughtForward":343.34,
+      |      "privateUseAdjustment":454.45,
+      |      "balancingCharge":231.45,
+      |      "periodOfGraceAdjustment":true,
+      |      "businessPremisesRenovationAllowanceBalancingCharges":567.67,
+      |      "nonResidentLandlord":true,
+      |      "rentARoom":{
+      |         "jointlyLet":true
+      |      }
+      |   }
+      |}
+      |""".stripMargin)
+
+  "reads" should {
+    "read JSON into a model" in {
+      downstreamJson.as[UkFhlProperty] shouldBe model
+    }
+  }
+
+  "writes" should {
+    "write a model to JSON" in {
+      Json.toJson(model) shouldBe mtdJson
+    }
+  }
+}

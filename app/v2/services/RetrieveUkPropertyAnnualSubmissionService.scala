6c621a5362867a701f8d39790135772795afe7a0
Jeremy Stone <6453521+jeremystone@users.noreply.github.com>
2021-10-25 13:53:35 +0100

    MTDSA-10094 - uk annual retrieval service and connector (#102)
    Co-authored-by: james-work-account <31282758+james-work-account@users.noreply.github.com>

---
+++ app/v2/services/RetrieveUkPropertyAnnualSubmissionService.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.services
+
+import cats.data.EitherT
+import cats.implicits._
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.Logging
+import v2.connectors.RetrieveUkPropertyAnnualSubmissionConnector
+import v2.connectors.RetrieveUkPropertyAnnualSubmissionConnector.{ NonUkResult, UkResult }
+import v2.controllers.EndpointLogContext
+import v2.models.errors._
+import v2.models.outcomes.ResponseWrapper
+import v2.models.request.retrieveUkPropertyAnnualSubmission.RetrieveUkPropertyAnnualSubmissionRequest
+import v2.models.response.retrieveUkPropertyAnnualSubmission.RetrieveUkPropertyAnnualSubmissionResponse
+import v2.support.IfsResponseMappingSupport
+
+import javax.inject.{ Inject, Singleton }
+import scala.concurrent.{ ExecutionContext, Future }
+
+@Singleton
+class RetrieveUkPropertyAnnualSubmissionService @Inject()(connector: RetrieveUkPropertyAnnualSubmissionConnector)
+    extends IfsResponseMappingSupport
+    with Logging {
+
+  private val ifsErrorMap =
+    Map(
+      "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
+      "INVALID_INCOMESOURCEID"    -> BusinessIdFormatError,
+      "INVALID_TAX_YEAR"          -> TaxYearFormatError,
+      "INVALID_CORRELATIONID"     -> DownstreamError,
+      "NO_DATA_FOUND"             -> NotFoundError,
+      "TAX_YEAR_NOT_SUPPORTED"    -> RuleTaxYearNotSupportedError,
+      "SERVER_ERROR"              -> DownstreamError,
+      "SERVICE_UNAVAILABLE"       -> DownstreamError
+    )
+
+  def retrieveUkProperty(request: RetrieveUkPropertyAnnualSubmissionRequest)(
+      implicit hc: HeaderCarrier,
+      ec: ExecutionContext,
+      logContext: EndpointLogContext,
+      correlationId: String): Future[ServiceOutcome[RetrieveUkPropertyAnnualSubmissionResponse]] = {
+
+    val result = for {
+      connectorResultWrapper <- EitherT(connector.retrieveUkProperty(request)).leftMap(mapIfsErrors(ifsErrorMap))
+      mtdResponseWrapper     <- EitherT.fromEither[Future](validateBusinessType(connectorResultWrapper))
+    } yield mtdResponseWrapper
+
+    result.value
+  }
+
+  private def validateBusinessType(resultWrapper: ResponseWrapper[RetrieveUkPropertyAnnualSubmissionConnector.Result]) =
+    resultWrapper.responseData match {
+      case UkResult(response) => Right(ResponseWrapper(resultWrapper.correlationId, response))
+      case NonUkResult        => Left(ErrorWrapper(resultWrapper.correlationId, RuleTypeOfBusinessIncorrect))
+    }
+
+}

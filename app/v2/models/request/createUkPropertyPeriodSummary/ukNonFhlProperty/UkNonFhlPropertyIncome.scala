0ddf951c1b09fc9d88879f7facd3f2d0a4a24073
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2021-11-09 09:31:17 +0000

    [MTDSA-9943] - models for create a Uk period summary (#117)
    * models for create a Uk period summary
    
    * [MTDSA-9943] Added consolidatedExpenses and amended rentARoom in expenses and income
    
    * [MTDSA-9943] updated ukFhlPropertyRentARoom
    
    * [MTDSA-9943]Renamed ukProperty objects
    
    Co-authored-by: vfryer12 <72739659+vfryer12@users.noreply.github.com>

---
+++ app/v2/models/request/createUkPropertyPeriodSummary/ukNonFhlProperty/UkNonFhlPropertyIncome.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.models.request.createUkPropertyPeriodSummary.ukNonFhlProperty
+
+import play.api.libs.json.{JsPath, Json, Reads, Writes}
+import play.api.libs.functional.syntax._
+import v2.models.request.common.ukPropertyRentARoom.UkPropertyIncomeRentARoom
+
+case class UkNonFhlPropertyIncome(premiumsOfLeaseGrant: Option[BigDecimal],
+                                  reversePremiums: Option[BigDecimal],
+                                  periodAmount: Option[BigDecimal],
+                                  taxDeducted: Option[BigDecimal],
+                                  otherIncome: Option[BigDecimal],
+                                  rentARoom: Option[UkPropertyIncomeRentARoom])
+
+
+object UkNonFhlPropertyIncome {
+  implicit val reads: Reads[UkNonFhlPropertyIncome] = Json.reads[UkNonFhlPropertyIncome]
+
+  implicit val writes: Writes[UkNonFhlPropertyIncome] = (
+    (JsPath \ "premiumsOfLeaseGrant").writeNullable[BigDecimal] and
+      (JsPath \ "reversePremiums").writeNullable[BigDecimal] and
+      (JsPath \ "periodAmount").writeNullable[BigDecimal] and
+      (JsPath \ "taxDeducted").writeNullable[BigDecimal] and
+      (JsPath \ "otherIncome").writeNullable[BigDecimal] and
+      (JsPath \ "ukOtherRentARoom").writeNullable[UkPropertyIncomeRentARoom]
+  ) (unlift(UkNonFhlPropertyIncome.unapply))
+
+}
---
+++ test/v2/models/request/createUkPropertyPeriodSummary/ukFhlProperty/UkFhlPropertyIncomeSpec.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.models.request.createUkPropertyPeriodSummary.ukFhlProperty
+
+import play.api.libs.json.{JsValue, Json}
+import support.UnitSpec
+import v2.models.request.common.ukPropertyRentARoom.UkPropertyIncomeRentARoom
+
+class UkFhlPropertyIncomeSpec extends UnitSpec {
+
+  val requestBody: UkFhlPropertyIncome =
+    UkFhlPropertyIncome(
+      Some(5000.99),
+      Some(3123.21),
+      Some(UkPropertyIncomeRentARoom(
+        Some(532.12)
+      ))
+    )
+
+  val mtdJson: JsValue = Json.parse(
+    """
+      |{
+      |    "periodAmount": 5000.99,
+      |    "taxDeducted": 3123.21,
+      |    "rentARoom": {
+      |        "rentsReceived": 532.12
+      |    }
+      |}
+      |""".stripMargin)
+
+
+  val desJson: JsValue = Json.parse(
+    """
+      |{
+      |    "periodAmount": 5000.99,
+      |    "taxDeducted": 3123.21,
+      |    "ukFhlRentARoom": {
+      |        "rentsReceived": 532.12
+      |    }
+      |}
+      |""".stripMargin)
+
+
+  "reads" when {
+    "passed a valid JSON" should {
+      "return a valid model" in {
+        mtdJson.as[UkFhlPropertyIncome] shouldBe requestBody
+      }
+    }
+  }
+  "writes" when {
+    "passed valid model" should {
+      "return valid JSON" in {
+        Json.toJson(requestBody) shouldBe desJson
+      }
+    }
+  }
+}

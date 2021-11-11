4f31ffbd96a513c1a3fcad87543d5e5277ea984b
Thepworth95 <56401451+Thepworth95@users.noreply.github.com>
2021-11-11 15:31:58 +0000

    [MTDSA-10059] - Create and amend a UK property business annual summary Modesl (#123)
    * amend uk prop annual models
    
    * amend uk prop models tests
    
    * make some objects optional
    
    * update tests

---
+++ app/v2/models/request/amendUkPropertyAnnualSubmission/ukFhlProperty/UkFhlPropertyAdjustments.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.models.request.amendUkPropertyAnnualSubmission.ukFhlProperty
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{JsPath, Json, Reads, Writes}
+import v2.models.request.common.ukPropertyRentARoom.UkPropertyAdjustmentsRentARoom
+
+case class UkFhlPropertyAdjustments(lossBroughtForward: Option[BigDecimal],
+                                    privateUseAdjustment: Option[BigDecimal],
+                                    balancingCharge: Option[BigDecimal],
+                                    periodOfGraceAdjustment: Boolean,
+                                    businessPremisesRenovationAllowanceBalancingCharges: Option[BigDecimal],
+                                    nonResidentLandlord: Boolean,
+                                    rentARoom: Option[UkPropertyAdjustmentsRentARoom])
+
+object UkFhlPropertyAdjustments {
+  implicit val reads: Reads[UkFhlPropertyAdjustments] = Json.reads[UkFhlPropertyAdjustments]
+
+  implicit val writes: Writes[UkFhlPropertyAdjustments] = (
+  (JsPath \ "lossBroughtForward").writeNullable[BigDecimal] and
+    (JsPath \ "privateUseAdjustment").writeNullable[BigDecimal] and
+    (JsPath \ "balancingCharge").writeNullable[BigDecimal] and
+    (JsPath \ "periodOfGraceAdjustment").write[Boolean] and
+    (JsPath \ "businessPremisesRenovationAllowanceBalancingCharges").writeNullable[BigDecimal] and
+    (JsPath \ "nonResidentLandlord").write[Boolean] and
+    (JsPath \ "ukFhlRentARoom").writeNullable[UkPropertyAdjustmentsRentARoom]
+  ) (unlift(UkFhlPropertyAdjustments.unapply))
+}

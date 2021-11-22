c1c488a20d5d7e340575b8ccc48fa622c6c8a982
DreamcastKnight <56401347+DreamcastKnight@users.noreply.github.com>
2021-11-22 11:34:52 +0000

    retrieve uk property periodic connector and service

---
+++ test/v2/connectors/RetrieveUkPropertyPeriodSummaryConnectorSpec.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.connectors
+
+import mocks.MockAppConfig
+import v2.mocks.MockHttpClient
+import v2.models.domain.Nino
+import v2.models.outcomes.ResponseWrapper
+
+import scala.concurrent.Future
+
+class RetrieveUkPropertyPeriodSummaryConnectorSpec extends ConnectorSpec {
+
+  val nino: String = "AA123456A"
+  val businessId: String = "XAIS12345678910"
+  val taxYear: String = "2019-20"
+  val submissionId: String = "4557ecb5-fd32-48cc-81f5-e6acd1099f3c"
+
+  val request: RetrieveUkPropertyPeriodSummaryRequest(
+  nino = Nino(nino),
+  businessId = businessId,
+  taxYear = taxYear,
+  submissionId = submissionId
+  )
+
+  private val response = RetrieveUkPropertyPeriodSummaryResponse
+
+  class Test extends MockHttpClient with MockAppConfig {
+    val connector: RetrieveUkPropertyPeriodSummaryConnector = new RetrieveUkPropertyPeriodSummaryConnector(
+      http = mockHttpClient,
+      appConfig = mockAppConfig
+    )
+
+    MockAppConfig.ifsBaseUrl returns baseUrl
+    MockAppConfig.ifsToken returns "ifs-token"
+    MockAppConfig.ifsEnvironment returns "ifs-environment"
+    MockAppConfig.ifsEnvironmentHeaders returns Some(allowedIfsHeaders)
+  }
+
+ "connector" must {
+   "send a request and return a body" in new Test {
+     val outcome = Right(ResponseWrapper(correlationId, response))
+
+     MockHttpClient
+       .get(
+         url = s"$baseUrl/income-tax/business/property/periodic?$nino/$businessId/$taxYear",
+         config = dummyIfsHeaderCarrierConfig,
+         requiredHeaders = requiredIfsHeaders,
+         excludedHeaders = Seq("AnotherHeader" -> "HeaderValue")
+       )
+       .returns(Future.successful(outcome))
+
+     await(connector.retrieveUkProperty(request)) shouldBe outcome
+   }
+ }
+}
---
+++ test/v2/services/RetrieveUkPropertyPeriodSummaryServiceSpec.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.services
+
+import support.UnitSpec
+import uk.gov.hmrc.http.HeaderCarrier
+import v2.controllers.EndpointLogContext
+import v2.mocks.connectors.MockRetrieveUkPropertyPeriodSummaryConnector
+import v2.models.domain.Nino
+import v2.models.errors._
+import v2.models.outcomes.ResponseWrapper
+
+import scala.concurrent.Future
+
+class RetrieveUkPropertyPeriodSummaryServiceSpec extends UnitSpec {
+
+  val nino: String = "AA123456A"
+  val businessId: String = "XAIS12345678910"
+  val taxYear: String = "2019-20"
+  val submissionId: String = "4557ecb5-fd32-48cc-81f5-e6acd1099f3c"
+  implicit val correlationId: String = "X-123"
+
+  private val response = RetrieveUkPropertyPeriodSummaryResponse
+
+
+  private val requestData = RetrieveUkPropertyPeriodSummaryRequest(Nino(nino), businessId, taxYear, submissionId)
+
+  trait Test extends MockRetrieveUkPropertyPeriodSummaryConnector {
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+    implicit val logContext: EndpointLogContext = EndpointLogContext("c", "ep")
+
+    val service = new RetrieveUkPropertyPeriodSummaryService(
+      connector = mockConnector
+    )
+  }
+
+  "service" should {
+    "service call successful" when {
+      "return mapped result" in new Test {
+        MockRetrieveUkPropertyConnector.retrieveUkProperty(requestData)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, response))))
+
+        await(service.retrieveUkProperty(requestData)) shouldBe Right(ResponseWrapper(correlationId, response))
+      }
+    }
+  }
+
+  "unsuccessful" should {
+    "map errors according to spec" when {
+
+      def serviceError(ifsErrorCode: String, error: MtdError): Unit =
+        s"a $ifsErrorCode error is returned from the service" in new Test {
+
+          MockRetrieveUkPropertyConnector.retrieveUkProperty(requestData)
+            .returns(Future.successful(Left(ResponseWrapper(correlationId, IfsErrors.single(IfsErrorCode(ifsErrorCode))))))
+
+          await(service.retrieveUkProperty(requestData)) shouldBe Left(ErrorWrapper(correlationId, error))
+        }
+
+      val input = Seq(
+        "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
+        "INVALID_TAX_YEAR" -> TaxYearFormatError,
+        "INVALID_INCOMESOURCE_ID" -> BusinessIdFormatError,
+        "INVALID_SUBMISSION_ID" -> SubmissionIdFormatError,
+        "TAX_YEAR_NOT_SUPPORTED" -> RuleTaxYearNotSupportedError,
+        "NO_DATA_FOUND" -> NotFoundError,
+        "SERVER_ERROR" -> DownstreamError,
+        "SERVICE_UNAVAILABLE" -> DownstreamError,
+        "INVALID_CORRELATIONID" -> DownstreamError
+      )
+
+      input.foreach(args => (serviceError _).tupled(args))
+    }
+  }
+}

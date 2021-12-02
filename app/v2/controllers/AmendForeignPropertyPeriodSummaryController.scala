704f81f6ea4187a7137e993268eef6a36be6b2b2
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2021-12-02 13:46:12 +0000

    [MTDSA-10552] - Amend a Foreign Property Income & Expenses Period Summary Controller (#171)
    * continued tests
    
    * its written
    
    * continued controller
    
    * amended controller
    
    * amended controller
    
    * amendment to routes
    
    * finished amendments to ITs
    
    * commented out unused route
    
    * small change to routes file
    
    * adressed comments

---
+++ app/v2/controllers/AmendForeignPropertyPeriodSummaryController.scala
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.controllers
+
+import cats.data.EitherT
+import cats.implicits._
+import javax.inject.{Inject, Singleton}
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, ControllerComponents}
+import utils.{IdGenerator, Logging}
+import v2.hateoas.HateoasFactory
+import v2.models.errors._
+import v2.models.response.amendForeignPropertyPeriodSummary.AmendForeignPropertyPeriodSummaryResponse.AmendForeignPropertyLinksFactory
+import v2.models.request.amendForeignPropertyPeriodSummary.AmendForeignPropertyPeriodSummaryRawData
+import v2.models.response.amendForeignPropertyPeriodSummary.AmendForeignPropertyPeriodSummaryHateoasData
+import v2.controllers.requestParsers.AmendForeignPropertyPeriodSummaryRequestParser
+import v2.services.{AmendForeignPropertyPeriodSummaryService, EnrolmentsAuthService, MtdIdLookupService}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class AmendForeignPropertyPeriodSummaryController @Inject()(val authService: EnrolmentsAuthService,
+                                                            val lookupService: MtdIdLookupService,
+                                                            parser: AmendForeignPropertyPeriodSummaryRequestParser,
+                                                            service: AmendForeignPropertyPeriodSummaryService,
+                                                            hateoasFactory: HateoasFactory,
+                                                            cc: ControllerComponents,
+                                                            idGenerator: IdGenerator)(implicit ec: ExecutionContext)
+  extends AuthorisedController(cc) with BaseController with Logging {
+
+
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(controllerName = "AmendForeignPropertyPeriodicController", endpointName = "amendForeignPropertyPeriodic")
+
+  def handleRequest(nino: String, businessId: String, taxYear: String, submissionId: String): Action[JsValue] =
+    authorisedAction(nino).async(parse.json) { implicit request =>
+      implicit val correlationId: String = idGenerator.getCorrelationId
+      logger.info(message = s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] " +
+        s"with correlationId : $correlationId")
+      val rawData = AmendForeignPropertyPeriodSummaryRawData(nino, businessId, taxYear, submissionId, request.body)
+      val result =
+      for {
+        parsedRequest <- EitherT.fromEither[Future](parser.parseRequest(rawData))
+        serviceResponse <- EitherT(service.amendForeignPropertyPeriodSummary(parsedRequest))
+        vendorResponse <- EitherT.fromEither[Future](
+          hateoasFactory.wrap(serviceResponse.responseData,
+            AmendForeignPropertyPeriodSummaryHateoasData(nino, businessId, taxYear, submissionId)).
+            asRight[ErrorWrapper]
+        )
+      } yield {
+        logger.info(
+          s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+            s"Success response received with CorrelationId: ${serviceResponse.correlationId}")
+
+        Ok(Json.toJson(vendorResponse))
+          .withApiHeaders(serviceResponse.correlationId)
+      }
+
+      result.leftMap { errorWrapper =>
+        val resCorrelationId = errorWrapper.correlationId
+        val result = errorResult(errorWrapper).withApiHeaders(resCorrelationId)
+
+        logger.warn(
+          s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+            s"Error response received with CorrelationId: $resCorrelationId")
+        result
+      }.merge
+    }
+
+
+  private def errorResult(errorWrapper: ErrorWrapper) = {
+    (errorWrapper.error: @unchecked) match {
+      case BadRequestError |
+           NinoFormatError |
+           TaxYearFormatError |
+           BusinessIdFormatError |
+           SubmissionIdFormatError |
+           CountryCodeFormatError |
+           FromDateFormatError |
+           ToDateFormatError |
+           RuleCountryCodeError |
+           RuleOverlappingPeriodError |
+           RuleMisalignedPeriodError |
+           RuleNotContiguousPeriodError |
+           RuleDuplicateSubmissionError |
+           RuleTaxYearRangeInvalidError |
+           RuleTaxYearNotSupportedError |
+           RuleIncorrectOrEmptyBodyError |
+           RuleTypeOfBusinessIncorrectError |
+           MtdErrorWithCustomMessage(ValueFormatError.code) |
+           MtdErrorWithCustomMessage(RuleBothExpensesSuppliedError.code) |
+           MtdErrorWithCustomMessage(RuleIncorrectOrEmptyBodyError.code) =>
+        BadRequest(Json.toJson(errorWrapper))
+      case NotFoundError => NotFound(Json.toJson(errorWrapper))
+      case DownstreamError => InternalServerError(Json.toJson(errorWrapper))
+    }
+  }
+}

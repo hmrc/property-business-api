4ef4d949655b1578ad051ff78da45206e457c04a
Samuel Jacobs <108878134+SamuelJacobsHMRC@users.noreply.github.com>
2022-07-21 15:11:10 +0100

    completed scaffolding - waiting on models ticket to be merged to main

--- test/v2/controllers/requestParsers/validators/RetrieveHistoricNonFhlUkPropertyAnnualSubmissionValidatorSpec.scala
+++ test/v2/controllers/requestParsers/validators/RetrieveHistoricNonFhlUkPropertyAnnualSubmissionValidatorSpec.scala
@@ -19,9 +19,8 @@ package v2.controllers.requestParsers.validators
 import mocks.MockAppConfig
 import support.UnitSpec
 import v2.models.errors._
-import v2.models.request.retrieveHistoricFhlUkPropertyAnnualSubmission.RetrieveHistoricFhlUkPropertyAnnualSubmissionRawData
 
-class RetrieveHistoricFhlUkPropertyAnnualSubmissionValidatorSpec extends UnitSpec with MockAppConfig {
+class RetrieveHistoricNonFhlUkPropertyAnnualSubmissionValidatorSpec extends UnitSpec with MockAppConfig {
 
   private val validNino    = "AA123456A"
   private val validTaxYear = "2021-22"
@@ -29,34 +28,34 @@ class RetrieveHistoricFhlUkPropertyAnnualSubmissionValidatorSpec extends UnitSpe
   MockAppConfig.minimumTaxHistoric returns 2017
   MockAppConfig.maximumTaxHistoric returns 2021
 
-  private val validator = new RetrieveHistoricFhlUkPropertyAnnualSubmissionValidator(mockAppConfig)
+  private val validator = new RetrieveHistoricNonFhlUkPropertyAnnualSubmissionValidator(mockAppConfig)
 
   "running a validation" should {
     "return no errors" when {
       "a valid request is supplied" in {
-        validator.validate(RetrieveHistoricFhlUkPropertyAnnualSubmissionRawData(validNino, validTaxYear)) shouldBe Nil
+        validator.validate(RetrieveHistoricNonFhlUkPropertyAnnualSubmissionRawData(validNino, validTaxYear)) shouldBe Nil
       }
     }
     "return a path parameter format error" when {
       "an invalid nino is supplied" in {
-        validator.validate(RetrieveHistoricFhlUkPropertyAnnualSubmissionRawData("Nino", validTaxYear)) shouldBe List(NinoFormatError)
+        validator.validate(RetrieveHistoricNonFhlUkPropertyAnnualSubmissionRawData("Nino", validTaxYear)) shouldBe List(NinoFormatError)
       }
       "an invalid taxYear format is supplied" in {
-        validator.validate(RetrieveHistoricFhlUkPropertyAnnualSubmissionRawData(validNino, "2021/22")) shouldBe List(TaxYearFormatError)
+        validator.validate(RetrieveHistoricNonFhlUkPropertyAnnualSubmissionRawData(validNino, "2021/22")) shouldBe List(TaxYearFormatError)
       }
       "an unsupported taxYear is supplied" in {
-        validator.validate(RetrieveHistoricFhlUkPropertyAnnualSubmissionRawData(validNino, "2016-17")) shouldBe List(
+        validator.validate(RetrieveHistoricNonFhlUkPropertyAnnualSubmissionRawData(validNino, "2016-17")) shouldBe List(
           RuleHistoricTaxYearNotSupportedError)
       }
       "an unsupported historic taxYear is supplied" in {
-        validator.validate(RetrieveHistoricFhlUkPropertyAnnualSubmissionRawData(validNino, "2022-23")) shouldBe List(
+        validator.validate(RetrieveHistoricNonFhlUkPropertyAnnualSubmissionRawData(validNino, "2022-23")) shouldBe List(
           RuleHistoricTaxYearNotSupportedError)
       }
       "an invalid taxYear range is supplied" in {
-        validator.validate(RetrieveHistoricFhlUkPropertyAnnualSubmissionRawData(validNino, "2021-23")) shouldBe List(RuleTaxYearRangeInvalidError)
+        validator.validate(RetrieveHistoricNonFhlUkPropertyAnnualSubmissionRawData(validNino, "2021-23")) shouldBe List(RuleTaxYearRangeInvalidError)
       }
       "multiple format errors are made" in {
-        validator.validate(RetrieveHistoricFhlUkPropertyAnnualSubmissionRawData("Nino", "2021/22")) shouldBe
+        validator.validate(RetrieveHistoricNonFhlUkPropertyAnnualSubmissionRawData("Nino", "2021/22")) shouldBe
           List(NinoFormatError, TaxYearFormatError)
       }
     }

7eeba5a1221539bf825269d7652bc981cf02a0fa
sean-flynn-HMRC <106095575+sean-flynn-HMRC@users.noreply.github.com>
2022-08-10 16:58:30 +0100

    Models started. PeriodExpenses unfinished.

--- app/v2/models/response/retrieveHistoricFhlUkPiePeriodSummary/PeriodExpenses.scala
+++ app/v2/models/response/retrieveHistoricFhlUkPiePeriodSummary/PeriodExpenses.scala
@@ -1,1 +1,30 @@
-package v2.models.response.retrieveHistoricFhlUkPiePeriodSummary case class PeriodExpenses ()
+package v2.models.response.retrieveHistoricFhlUkPiePeriodSummary
+
+import play.api.libs.functional.syntax.toFunctionalBuilderOps
+import play.api.libs.json.{JsPath, Json, OWrites, Reads}
+
+case class PeriodExpenses (premiseRunningCosts: Option[BigDecimal],
+                           repairsAndMaintenance: Option[BigDecimal],
+                           financialCosts: Option[BigDecimal],
+                           professionalFees: Option[BigDecimal],
+                           costOfServices: Option[BigDecimal],
+                           other: Option[BigDecimal],
+                           consolidatedExpenses: Option[BigDecimal],
+                           travelCosts: Option[BigDecimal],
+                           rentARoom: Option[Float])
+
+case object PeriodExpenses{
+  implicit val writes: OWrites[PeriodExpenses] = Json.writes[PeriodExpenses]
+
+  implicit val reads: Reads[PeriodExpenses] = (
+    (JsPath \ "premiseRunningCosts").readNullable[BigDecimal] and
+      (JsPath \ "repairsAndMaintenance").readNullable[BigDecimal] and
+      (JsPath \ "financialCosts").readNullable[BigDecimal] and
+      (JsPath \ "professionalFees").readNullable[BigDecimal] and
+      (JsPath \ "costOfServices").readNullable[BigDecimal] and
+      (JsPath \ "other").readNullable[BigDecimal] and
+      (JsPath \ "consolidatedExpenses").readNullable[BigDecimal] and
+      (JsPath \ "travelCosts").readNullable[BigDecimal] and
+      (JsPath \ "rentARoom").readNullable[Float]
+    ) (PeriodExpenses.apply _)
+}
--- app/v2/models/response/retrieveHistoricFhlUkPiePeriodSummary/PeriodIncome.scala
+++ app/v2/models/response/retrieveHistoricFhlUkPiePeriodSummary/PeriodIncome.scala
@@ -1,1 +1,33 @@
-package v2.models.response.retrieveHistoricFhlUkPiePeriodSummary case class periodIncome ()
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.models.response.retrieveHistoricFhlUkPiePeriodSummary
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{JsPath, Json, OWrites, Reads}
+
+case class PeriodIncome(amount: Option[BigDecimal], taxDeducted: Option[BigDecimal], rentARoom: Option[RentARoom])
+
+case object PeriodIncome{
+  implicit val writes: OWrites[PeriodIncome] = Json.writes[PeriodIncome]
+
+  implicit val reads: Reads[PeriodIncome] = (
+    (JsPath \ "amount").readNullable[BigDecimal] and
+      (JsPath \ "taxDeducted").readNullable[BigDecimal] and
+      (JsPath \ "rentARoom").readNullable[RentARoom]
+    ) (PeriodIncome.apply _)
+
+}

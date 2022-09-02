c0a982078f7ca3ff70c375fe3a0e095f17965d26
SamuelJacobsHMRC <108878134+SamuelJacobsHMRC@users.noreply.github.com>
2022-09-02 10:35:55 +0100

    MTDSA-12498: Controller for Retrieve Historic FHL UK Period Income and Expenses Period Summary (#285)
    * MTDSA-12498: Began scaffolding controller and spec
    
    * MTDSA-12398: Started writing ISpec
    
    * MTDSA-12498: Continued with scaffolding
    
    * MTDSA-12498: Completed scaffolding - awaiting connector and service
    
    * MTDSA-12498: Completed ISpec
    
    * MTDSA-12498: Implemented review feedback

---
+++ app/v2/controllers/RetrieveHistoricFhlUkPiePeriodSummaryController.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.controllers
+
+import cats.data.EitherT
+import cats.implicits._
+import play.api.libs.json.Json
+import play.api.mvc.{ Action, AnyContent, ControllerComponents }
+import utils.{ IdGenerator, Logging }
+import v2.controllers.requestParsers.RetrieveHistoricFhlUkPropertyPeriodSummaryRequestParser
+import v2.hateoas.HateoasFactory
+import v2.models.errors._
+import v2.models.request.retrieveHistoricFhlUkPiePeriodSummary.RetrieveHistoricFhlUkPiePeriodSummaryRawData
+import v2.models.response.retrieveHistoricFhlUkPiePeriodSummary.RetrieveHistoricFhlUkPiePeriodSummaryHateoasData
+import v2.services.{ EnrolmentsAuthService, MtdIdLookupService, RetrieveHistoricFhlUkPropertyPeriodSummaryService }
+
+import javax.inject.{ Inject, Singleton }
+import scala.concurrent.{ ExecutionContext, Future }
+
+@Singleton
+class RetrieveHistoricFhlUkPiePeriodSummaryController @Inject()(val authService: EnrolmentsAuthService,
+                                                                val lookupService: MtdIdLookupService,
+                                                                parser: RetrieveHistoricFhlUkPropertyPeriodSummaryRequestParser,
+                                                                service: RetrieveHistoricFhlUkPropertyPeriodSummaryService,
+                                                                hateoasFactory: HateoasFactory,
+                                                                cc: ControllerComponents,
+                                                                idGenerator: IdGenerator)(implicit ec: ExecutionContext)
+    extends AuthorisedController(cc)
+    with BaseController
+    with Logging {
+
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(controllerName = "RetrieveHistoricFhlUkPiePeriodSummaryController",
+                       endpointName = "retrieveHistoricFhlUkPropertyPeriodSummary")
+
+  def handleRequest(nino: String, periodId: String): Action[AnyContent] =
+    authorisedAction(nino).async { implicit request =>
+      implicit val correlationId: String = idGenerator.getCorrelationId
+      logger.info(
+        message = s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] " +
+          s"with correlationId : $correlationId")
+      val rawData = RetrieveHistoricFhlUkPiePeriodSummaryRawData(nino, periodId)
+      val result =
+        for {
+          parsedRequest   <- EitherT.fromEither[Future](parser.parseRequest(rawData))
+          serviceResponse <- EitherT(service.retrieve(parsedRequest))
+          vendorResponse <- EitherT.fromEither[Future](
+            hateoasFactory
+              .wrap(serviceResponse.responseData, RetrieveHistoricFhlUkPiePeriodSummaryHateoasData(nino, periodId))
+              .asRight[ErrorWrapper]
+          )
+        } yield {
+          logger.info(
+            s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+              s"Success response received with CorrelationId: ${serviceResponse.correlationId}")
+
+          Ok(Json.toJson(vendorResponse))
+            .withApiHeaders(serviceResponse.correlationId)
+        }
+      result.leftMap { errorWrapper =>
+        val resCorrelationId = errorWrapper.correlationId
+        val result           = errorResult(errorWrapper).withApiHeaders(resCorrelationId)
+
+        logger.warn(
+          s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+            s"Error response received with CorrelationId: $resCorrelationId")
+        result
+      }.merge
+    }
+
+  private def errorResult(errorWrapper: ErrorWrapper) = {
+    errorWrapper.error match {
+      case NinoFormatError | PeriodIdFormatError | BadRequestError =>
+        BadRequest(Json.toJson(errorWrapper))
+      case InternalError => InternalServerError(Json.toJson(errorWrapper))
+      case NotFoundError => NotFound(Json.toJson(errorWrapper))
+      case _             => unhandledError(errorWrapper)
+    }
+  }
+}

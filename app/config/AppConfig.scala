3084043670bbf73806273a95994210d696b1a65f
jeremystone <6453521+jeremystone@users.noreply.github.com>
2022-10-31 14:24:26 +0000

    MTDSA-13375 - rename and update FeatureSwitches

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -41,7 +41,7 @@ trait AppConfig {
   def apiGatewayContext: String
   def confidenceLevelConfig: ConfidenceLevelConfig
   def apiStatus(version: String): String
-  def featureSwitch: Option[Configuration]
+  def featureSwitches: Configuration
   def endpointsEnabled(version: String): Boolean
 
   def minimumTaxV2Foreign: Int
@@ -73,7 +73,7 @@ class AppConfigImpl @Inject()(config: ServicesConfig, configuration: Configurati
   val apiGatewayContext: String                    = config.getString("api.gateway.context")
   val confidenceLevelConfig: ConfidenceLevelConfig = configuration.get[ConfidenceLevelConfig](s"api.confidence-level-check")
   def apiStatus(version: String): String           = config.getString(s"api.$version.status")
-  def featureSwitch: Option[Configuration]         = configuration.getOptional[Configuration](s"feature-switch")
+  def featureSwitches: Configuration = configuration.getOptional[Configuration](s"feature-switch").getOrElse(Configuration.empty)
   def endpointsEnabled(version: String): Boolean   = config.getBoolean(s"api.$version.endpoints.enabled")
 
   val minimumTaxV2Foreign: Int = config.getInt("minimum-tax-year.version-2.foreign")
--- app/config/FeatureSwitches.scala
+++ app/config/FeatureSwitches.scala
@@ -18,28 +18,29 @@ package config
 
 import play.api.Configuration
 
-case class FeatureSwitch(value: Option[Configuration]) {
+
+case class FeatureSwitches(featureSwitchConfig: Configuration) {
 
   private val versionRegex = """(\d)\.\d""".r
 
   def isVersionEnabled(version: String): Boolean = {
-    val versionNoIfPresent: Option[String] =
+    val maybeVersion: Option[String] =
       version match {
         case versionRegex(v) => Some(v)
         case _               => None
       }
 
     val enabled = for {
-      versionNo <- versionNoIfPresent
-      config    <- value
-      enabled   <- config.getOptional[Boolean](s"version-$versionNo.enabled")
+      validVersion <- maybeVersion
+      enabled      <- featureSwitchConfig.getOptional[Boolean](s"version-$validVersion.enabled")
     } yield enabled
 
     enabled.getOrElse(false)
   }
 
-  val isV2R7cRoutingEnabled: Boolean = value match {
-    case Some(config) => config.getOptional[Boolean]("v2r7c-endpoints.enabled").getOrElse(true)
-    case None         => true
-  }
+  val isV2R7cRoutingEnabled: Boolean = isEnabled("v2r7c-endpoints.enabled")
+  val isTaxYearSpecificApiEnabled: Boolean      = isEnabled("tys-api.enabled")
+
+  private def isEnabled(key: String): Boolean = featureSwitchConfig.getOptional[Boolean](key).getOrElse(true)
+
 }

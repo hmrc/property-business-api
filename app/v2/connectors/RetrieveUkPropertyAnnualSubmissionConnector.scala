5143db96e1da916f536262c37b67847b3f9ab358
Darshan Balasingam <112945711+darshanbala-hmrc@users.noreply.github.com>
2022-11-14 15:18:14 +0000

    MTDSA-13509 Updated Connector

--- app/v2/connectors/RetrieveUkPropertyAnnualSubmissionConnector.scala
+++ app/v2/connectors/RetrieveUkPropertyAnnualSubmissionConnector.scala
@@ -18,7 +18,7 @@ package v2.connectors
 
 import config.AppConfig
 import uk.gov.hmrc.http.{ HeaderCarrier, HttpClient }
-import v2.connectors.DownstreamUri.IfsUri
+import v2.connectors.DownstreamUri.{ IfsUri, TaxYearSpecificIfsUri }
 import v2.connectors.httpparsers.StandardDownstreamHttpParser._
 import v2.models.outcomes.ResponseWrapper
 import v2.models.request.retrieveUkPropertyAnnualSubmission.RetrieveUkPropertyAnnualSubmissionRequest
@@ -45,10 +45,24 @@ class RetrieveUkPropertyAnnualSubmissionConnector @Inject()(val http: HttpClient
                                                                              ec: ExecutionContext,
                                                                              correlationId: String): Future[DownstreamOutcome[Result]] = {
     // Note that MTD tax year format is used
-    val response = get(
-      uri = IfsUri[RetrieveUkPropertyAnnualSubmissionResponse]("income-tax/business/property/annual"),
-      queryParams = Seq("taxableEntityId" -> request.nino.value, "incomeSourceId" -> request.businessId, "taxYear" -> request.taxYear.asMtd)
-    )
+//    val response = get(
+//      uri = IfsUri[RetrieveUkPropertyAnnualSubmissionResponse]("income-tax/business/property/annual"),
+//      queryParams = Seq("taxableEntityId" -> request.nino.value, "incomeSourceId" -> request.businessId, "taxYear" -> request.taxYear.asMtd)
+//    )
+
+    import request._
+
+    val requestUri = if (taxYear.useTaxYearSpecificApi) {
+      TaxYearSpecificIfsUri[RetrieveUkPropertyAnnualSubmissionResponse](
+        s"income-tax/business/property/annual/${taxYear.asTysDownstream}/${nino.nino}/${businessId}")
+    } else {
+      // Note that MTD tax year format is used
+      IfsUri[RetrieveUkPropertyAnnualSubmissionResponse](
+        s"income-tax/business/property/annual" +
+          s"taxableEntityId=${nino.nino}&taxYear=${taxYear.asMtd}&incomeSourceId=$businessId")
+    }
+
+    val response = get(requestUri)
 
     response.map {
       case Right(ResponseWrapper(corId, resp)) if ukResult(resp) => Right(ResponseWrapper(corId, UkResult(resp)))

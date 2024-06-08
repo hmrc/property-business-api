6fa4ce760600dd2478a3839ec0a20e0ae40983ac
Michael Olafisoye <86134318+molafisoye@users.noreply.github.com>
2024-06-08 20:27:18 +0100

    Update app/api/support/DateUtils.scala
    Co-authored-by: Jeremy Stone <6453521+jeremystone@users.noreply.github.com>

--- app/api/support/DateUtils.scala
+++ app/api/support/DateUtils.scala
@@ -36,17 +36,6 @@ class DateUtils {
     .ofPattern("EEE, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH)
     .withZone(ZoneId.of("GMT"))
 
-  def getTaxYear(taxYearOpt: Option[String], currentDate: LocalDate): String = taxYearOpt match {
-    case Some(taxYear) => taxYear
-    case None =>
-      val fiscalYearStartDate = LocalDate.parse(s"${currentDate.getYear.toString}-04-05")
-
-      if (currentDate.isAfter(fiscalYearStartDate)) {
-        s"${currentDate.getYear}-${currentDate.getYear.+(1).toString.drop(2)}"
-      } else {
-        s"${currentDate.getYear.-(1)}-${currentDate.getYear.toString.drop(2)}"
-      }
-  }
 
   def longDateTimestampGmt(dateTime: LocalDateTime): String = longDateTimeFormatGmt.format(dateTime)
 }

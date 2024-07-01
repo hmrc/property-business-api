9d0be227287d1c6c73e25f56b47bc51bc948f6af
MadStu <737342+MadStu@users.noreply.github.com>
2024-07-01 14:15:26 +0100

    [MTDSA-24851] Update Depreecation Status and Tests

--- it/config/DocumentationControllerISpec.scala
+++ it/config/DocumentationControllerISpec.scala
@@ -63,7 +63,7 @@ class DocumentationControllerISpec extends IntegrationBaseSpec {
        |         },
        |         {
        |            "version":"3.0",
-       |            "status":"BETA",
+       |            "status":"DEPRECATED",
        |            "endpointsEnabled":true
        |         },
        |         {

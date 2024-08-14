15bbe0d42c410100e16d851469db7388a3ee0e1a
Kritika Rai <111003013+kritikarai6@users.noreply.github.com>
2024-08-14 10:35:33 +0100

    MTDSA-25345: change to remove fhl from retrieve foreign property annual submission endpoint

---
+++ app/v5/retrieveForeignPropertyAnnualSubmission/def2/Def2_RetrieveForeignPropertyAnnualSubmissionValidator.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v5.retrieveForeignPropertyAnnualSubmission.def2
+
+import api.controllers.validators.Validator
+import api.controllers.validators.resolvers.{ResolveBusinessId, ResolveNino, ResolveTaxYear}
+import api.models.errors.MtdError
+import cats.data.Validated
+import cats.implicits._
+import config.AppConfig
+import v5.retrieveForeignPropertyAnnualSubmission.def2.request.Def2_RetrieveForeignPropertyAnnualSubmissionRequestData
+import v5.retrieveForeignPropertyAnnualSubmission.model.request.RetrieveForeignPropertyAnnualSubmissionRequestData
+
+import javax.inject.Inject
+
+class Def2_RetrieveForeignPropertyAnnualSubmissionValidator @Inject()(nino: String, businessId: String, taxYear: String)(appConfig: AppConfig)
+    extends Validator[RetrieveForeignPropertyAnnualSubmissionRequestData] {
+
+  private lazy val minimumTaxYear = appConfig.minimumTaxV2Foreign
+
+  def validate: Validated[Seq[MtdError], RetrieveForeignPropertyAnnualSubmissionRequestData] =
+    (
+      ResolveNino(nino),
+      ResolveBusinessId(businessId),
+      ResolveTaxYear(minimumTaxYear, taxYear)
+    ).mapN(Def2_RetrieveForeignPropertyAnnualSubmissionRequestData)
+
+}
---
+++ app/v5/retrieveForeignPropertyAnnualSubmission/def2/model/response/Def2_RetrieveForeignPropertyAnnualSubmissionResponse.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v5.retrieveForeignPropertyAnnualSubmission.def2.model.response
+
+import api.models.domain.Timestamp
+import play.api.libs.functional.syntax.toFunctionalBuilderOps
+import play.api.libs.json.{JsPath, Json, OWrites, Reads}
+import v5.retrieveForeignPropertyAnnualSubmission.def2.model.response.def2_foreignProperty.Def2_Retrieve_ForeignPropertyEntry
+import v5.retrieveForeignPropertyAnnualSubmission.model.response.RetrieveForeignPropertyAnnualSubmissionResponse
+
+case class Def2_RetrieveForeignPropertyAnnualSubmissionResponse(
+                                                                 submittedOn: Timestamp,
+                                                                 foreignNonFhlProperty: Option[Seq[Def2_Retrieve_ForeignPropertyEntry]]
+                                                               ) extends RetrieveForeignPropertyAnnualSubmissionResponse{
+
+  override def isForeignResult: Boolean = foreignNonFhlProperty.nonEmpty
+}
+
+object Def2_RetrieveForeignPropertyAnnualSubmissionResponse {
+
+  implicit val writes: OWrites[Def2_RetrieveForeignPropertyAnnualSubmissionResponse] =
+    Json.writes[Def2_RetrieveForeignPropertyAnnualSubmissionResponse]
+
+  implicit val reads: Reads[Def2_RetrieveForeignPropertyAnnualSubmissionResponse] = (
+    (JsPath \ "submittedOn").read[Timestamp] and
+      (JsPath \ "foreignProperty").readNullable[Seq[Def2_Retrieve_ForeignPropertyEntry]]
+    )(Def2_RetrieveForeignPropertyAnnualSubmissionResponse.apply _)
+
+}

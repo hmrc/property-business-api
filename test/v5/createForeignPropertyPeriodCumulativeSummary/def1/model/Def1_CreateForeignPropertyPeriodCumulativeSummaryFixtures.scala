135b6ca6e66664eadea78a8ec39c7102fdfe6393
Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>
2024-10-10 12:02:39 +0100

    MTDSA-26690: Adds and fixes unit tests for new version.

---
+++ test/v5/createForeignPropertyPeriodCumulativeSummary/def1/model/Def1_CreateForeignPropertyPeriodCumulativeSummaryFixtures.scala
@@ -0,0 +1,210 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v5.createForeignPropertyPeriodCumulativeSummary.def1.model
+
+import play.api.libs.json.{JsValue, Json}
+import v5.createForeignPropertyPeriodCumulativeSummary.def1.model.request.Def1_foreignPropertyEntry._
+import v5.createForeignPropertyPeriodCumulativeSummary.model.request.Def1_CreateForeignPropertyPeriodCumulativeSummaryRequestBody
+
+trait Def1_CreateForeignPropertyPeriodCumulativeSummaryFixtures {
+
+  private val foreignPropertyIncome = Some(
+    Def1_Create_ForeignPropertyIncome(
+      rentIncome = Some(Def1_Create_ForeignPropertyRentIncome(Some(1000.99))),
+      foreignTaxCreditRelief = Some(false),
+      premiumsOfLeaseGrant = Some(1000.99),
+      otherPropertyIncome = Some(2000.99),
+      foreignTaxPaidOrDeducted = Some(3000.99),
+      specialWithholdingTaxOrUkTaxPaid = Some(4000.99)
+    ))
+
+  private val regularForeignPropertyExpenses = Some(
+    Def1_Create_CreateForeignPropertyExpenses(
+      premisesRunningCosts = Some(1000.99),
+      repairsAndMaintenance = Some(2000.99),
+      financialCostsAmount = Some(3000.99),
+      professionalFeesAmount = Some(4000.99),
+      costOfServicesAmount = Some(5000.99),
+      travelCostsAmount = Some(6000.99),
+      residentialFinancialCostAmount = Some(7000.99),
+      broughtFwdResidentialFinancialCostAmount = Some(8000.99),
+      otherAmount = Some(9000.99),
+      consolidatedExpenseAmount = None
+    ))
+
+  private val consolidatedForeignPropertyExpenses = Some(
+    Def1_Create_CreateForeignPropertyExpenses(
+      premisesRunningCosts = None,
+      repairsAndMaintenance = None,
+      financialCostsAmount = None,
+      professionalFeesAmount = None,
+      costOfServicesAmount = None,
+      travelCostsAmount = None,
+      residentialFinancialCostAmount = None,
+      broughtFwdResidentialFinancialCostAmount = None,
+      otherAmount = None,
+      consolidatedExpenseAmount = Some(1000.99)
+    ))
+
+  private val regularForeignProperty = Some(
+    List(Def1_Create_CreateForeignPropertyEntry(countryCode = "FRA", income = foreignPropertyIncome, expenses = regularForeignPropertyExpenses)))
+
+  private val consolidatedForeignProperty = Some(
+    List(
+      Def1_Create_CreateForeignPropertyEntry(
+        countryCode = "FRA",
+        income = foreignPropertyIncome,
+        expenses = consolidatedForeignPropertyExpenses
+      )))
+
+  val regularExpensesRequestBody: Def1_CreateForeignPropertyPeriodCumulativeSummaryRequestBody =
+    Def1_CreateForeignPropertyPeriodCumulativeSummaryRequestBody(
+      fromDate = "2025-01-01",
+      toDate = "2026-01-31",
+      foreignProperty = regularForeignProperty
+    )
+
+  val consolidatedExpensesRequestBody: Def1_CreateForeignPropertyPeriodCumulativeSummaryRequestBody =
+    Def1_CreateForeignPropertyPeriodCumulativeSummaryRequestBody(
+      fromDate = "2025-01-01",
+      toDate = "2026-01-31",
+      foreignProperty = consolidatedForeignProperty
+    )
+
+  val regularMtdRequestJson: JsValue = Json.parse(
+    """{
+      |   "fromDate": "2025-01-01",
+      |   "toDate": "2026-01-31",
+      |   "foreignProperty": [
+      |      {
+      |         "countryCode": "FRA",
+      |         "income":{
+      |            "rentIncome":{
+      |               "rentAmount": 1000.99
+      |            },
+      |            "foreignTaxCreditRelief": false,
+      |            "premiumsOfLeaseGrant": 1000.99,
+      |            "otherPropertyIncome": 2000.99,
+      |            "foreignTaxPaidOrDeducted": 3000.99,
+      |            "specialWithholdingTaxOrUkTaxPaid": 4000.99
+      |         },
+      |         "expenses": {
+      |            "premisesRunningCosts": 1000.99,
+      |            "repairsAndMaintenance": 2000.99,
+      |            "financialCostsAmount": 3000.99,
+      |            "professionalFeesAmount": 4000.99,
+      |            "costOfServicesAmount": 5000.99,
+      |            "travelCostsAmount": 6000.99,
+      |            "residentialFinancialCostAmount": 7000.99,
+      |            "broughtFwdResidentialFinancialCostAmount": 8000.99,
+      |            "otherAmount": 9000.99
+      |         }
+      |      }
+      |   ]
+      |}
+    """.stripMargin
+  )
+
+  val consolidatedMtdRequestJson: JsValue = Json.parse(
+    """{
+      |   "fromDate": "2025-01-01",
+      |   "toDate": "2026-01-31",
+      |   "foreignProperty": [
+      |      {
+      |         "countryCode": "FRA",
+      |         "income":{
+      |            "rentIncome":{
+      |               "rentAmount": 1000.99
+      |            },
+      |            "foreignTaxCreditRelief": false,
+      |            "premiumsOfLeaseGrant": 1000.99,
+      |            "otherPropertyIncome": 2000.99,
+      |            "foreignTaxPaidOrDeducted": 3000.99,
+      |            "specialWithholdingTaxOrUkTaxPaid": 4000.99
+      |         },
+      |         "expenses": {
+      |            "consolidatedExpenseAmount": 1000.99
+      |         }
+      |      }
+      |   ]
+      |}
+    """.stripMargin
+  )
+
+  val regularDownstreamRequestJson: JsValue = Json.parse(
+    """
+      |{
+      |  "fromDate": "2025-01-01",
+      |  "toDate": "2026-01-31",
+      |  "foreignProperty": [
+      |    {
+      |      "countryCode": "FRA",
+      |      "income": {
+      |        "rentIncome": {
+      |          "rentAmount": 1000.99
+      |        },
+      |        "foreignTaxCreditRelief": false,
+      |        "premiumsOfLeaseGrant": 1000.99,
+      |        "otherPropertyIncome": 2000.99,
+      |        "foreignTaxPaidOrDeducted": 3000.99,
+      |        "specialWithholdingTaxOrUkTaxPaid": 4000.99
+      |      },
+      |      "expenses": {
+      |        "premisesRunningCosts": 1000.99,
+      |        "repairsAndMaintenance": 2000.99,
+      |        "financialCostsAmount": 3000.99,
+      |        "professionalFeesAmount": 4000.99,
+      |        "costOfServicesAmount": 5000.99,
+      |        "travelCostsAmount": 6000.99,
+      |        "residentialFinancialCostAmount": 7000.99,
+      |        "broughtFwdResidentialFinancialCostAmount": 8000.99,
+      |        "otherAmount": 9000.99
+      |      }
+      |    }
+      |  ]
+      |}
+    """.stripMargin
+  )
+
+  val consolidatedDownstreamRequestJson: JsValue = Json.parse(
+    """
+      |{
+      |  "fromDate": "2025-01-01",
+      |  "toDate": "2026-01-31",
+      |  "foreignProperty": [
+      |    {
+      |      "countryCode": "FRA",
+      |      "income": {
+      |        "rentIncome": {
+      |          "rentAmount": 1000.99
+      |        },
+      |        "foreignTaxCreditRelief": false,
+      |        "premiumsOfLeaseGrant": 1000.99,
+      |        "otherPropertyIncome": 2000.99,
+      |        "foreignTaxPaidOrDeducted": 3000.99,
+      |        "specialWithholdingTaxOrUkTaxPaid": 4000.99
+      |      },
+      |      "expenses": {
+      |        "consolidatedExpenseAmount": 1000.99
+      |      }
+      |    }
+      |  ]
+      |}
+    """.stripMargin
+  )
+
+}
---
+++ test/v5/createForeignPropertyPeriodCumulativeSummary/model/response/CreateForeignPropertyPeriodCumulativeSummaryResponseSpec.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v5.createForeignPropertyPeriodCumulativeSummary.model.response
+
+import play.api.libs.json.Json
+import support.UnitSpec
+
+class CreateForeignPropertyPeriodCumulativeSummaryResponseSpec extends UnitSpec {
+
+  private val submissionId = "4557ecb5-fd32-48cc-81f5-e6acd1099f3c"
+
+  private val createForeignPropertyResponse = CreateForeignPropertyPeriodCumulativeSummaryResponse(submissionId)
+
+  private val json = Json.parse(s"""{
+                                  |  "submissionId": "$submissionId"
+                                  |}""".stripMargin)
+
+  "reads" when {
+    "passed a valid JSON" should {
+      "return a valid model" in {
+        json.as[CreateForeignPropertyPeriodCumulativeSummaryResponse] shouldBe createForeignPropertyResponse
+      }
+    }
+  }
+
+  "writes" when {
+    "passed valid model" should {
+      "return valid JSON" in {
+        Json.toJson(createForeignPropertyResponse) shouldBe json
+      }
+    }
+  }
+
+}

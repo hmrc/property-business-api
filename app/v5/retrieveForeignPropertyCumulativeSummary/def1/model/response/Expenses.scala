e0d15cf91631fbd688ac442ddb37ca5a7ee88efc
Darshan Balasingam <112945711+darshanbala-hmrc@users.noreply.github.com>
2024-10-11 09:24:47 +0100

    MTDSA-26861: Code completed

---
+++ app/v5/retrieveForeignPropertyCumulativeSummary/def1/model/response/Expenses.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v5.retrieveForeignPropertyCumulativeSummary.def1.model.response
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{JsPath, Json, OWrites, Reads}
+
+case class Expenses(premisesRunningCosts: Option[BigDecimal],
+                    repairsAndMaintenance: Option[BigDecimal],
+                    financialCosts: Option[BigDecimal],
+                    professionalFees: Option[BigDecimal],
+                    travelCosts: Option[BigDecimal],
+                    costOfServices: Option[BigDecimal],
+                    residentialFinancialCost: Option[BigDecimal],
+                    broughtFwdResidentialFinancialCost: Option[BigDecimal],
+                    other: Option[BigDecimal],
+                    consolidatedExpenses: Option[BigDecimal]) {}
+
+object Expenses {
+  implicit val writes: OWrites[Expenses] = Json.writes[Expenses]
+
+  implicit val reads: Reads[Expenses] = (
+    (JsPath \ "premisesRunningCostsAmount").readNullable[BigDecimal] and
+      (JsPath \ "repairsAndMaintenanceAmount").readNullable[BigDecimal] and
+      (JsPath \ "financialCostsAmount").readNullable[BigDecimal] and
+      (JsPath \ "professionalFeesAmount").readNullable[BigDecimal] and
+      (JsPath \ "travelCostsAmount").readNullable[BigDecimal] and
+      (JsPath \ "costOfServicesAmount").readNullable[BigDecimal] and
+      (JsPath \ "residentialFinancialCostAmount").readNullable[BigDecimal] and
+      (JsPath \ "broughtFwdResidentialFinancialCostAmount").readNullable[BigDecimal] and
+      (JsPath \ "otherAmount").readNullable[BigDecimal] and
+      (JsPath \ "consolidatedExpenses").readNullable[BigDecimal]
+  )(Expenses.apply _)
+
+}

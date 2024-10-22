0e851bcb9f60bf8ebb61b4c526f81d526ca214a5
Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>
2024-10-22 14:01:40 +0100

    MTDSA-26690: Rephrases tests to be more logical.

--- test/v5/createAmendForeignPropertyCumulativePeriodSummary/CreateAmendForeignPropertyCumulativePeriodSummaryServiceSpec.scala
+++ test/v5/createAmendForeignPropertyCumulativePeriodSummary/CreateAmendForeignPropertyCumulativePeriodSummaryServiceSpec.scala
@@ -45,57 +45,54 @@ class CreateAmendForeignPropertyCumulativePeriodSummaryServiceSpec
   private val service = new CreateAmendForeignPropertyCumulativePeriodSummaryService(mockCreateAmendForeignPropertyCumulativePeriodSummaryConnector)
 
   "service" should {
-    "service call successful" when {
-      "return mapped result" in {
+    "be successful" when {
+      "given a valid request" in {
         MockedCreateAmendForeignPropertyCumulativePeriodSummaryConnector
           .createForeignProperty(requestData)
           .returns(Future.successful(Right(ResponseWrapper(correlationId, responseData))))
 
-        val result = await(service.createForeignProperty(requestData))
+        val result = await(service.createAmendForeignProperty(requestData))
         result shouldBe Right(ResponseWrapper(correlationId, responseData))
       }
     }
-  }
+      "be unsuccessful and map errors according to spec" when {
+        def serviceError(downstreamErrorCode: String, error: MtdError): Unit =
+          s"a $downstreamErrorCode error is returned from the service" in {
 
-  "unsuccessful" should {
-    "map errors according to spec" when {
-      def serviceError(downstreamErrorCode: String, error: MtdError): Unit =
-        s"a $downstreamErrorCode error is returned from the service" in {
+            MockedCreateAmendForeignPropertyCumulativePeriodSummaryConnector
+              .createForeignProperty(requestData)
+              .returns(Future.successful(Left(ResponseWrapper(correlationId, DownstreamErrors.single(DownstreamErrorCode(downstreamErrorCode))))))
 
-          MockedCreateAmendForeignPropertyCumulativePeriodSummaryConnector
-            .createForeignProperty(requestData)
-            .returns(Future.successful(Left(ResponseWrapper(correlationId, DownstreamErrors.single(DownstreamErrorCode(downstreamErrorCode))))))
+            val result = await(service.createAmendForeignProperty(requestData))
+            result shouldBe Left(ErrorWrapper(correlationId, error))
+          }
 
-          val result = await(service.createForeignProperty(requestData))
-          result shouldBe Left(ErrorWrapper(correlationId, error))
-        }
+        val errors = List(
+          "INVALID_TAXABLE_ENTITY_ID"          -> NinoFormatError,
+          "INVALID_INCOME_SOURCE_ID"           -> BusinessIdFormatError,
+          "INVALID_PAYLOAD"                    -> InternalError,
+          "INVALID_CORRELATION_ID"             -> InternalError,
+          "INVALID_TAX_YEAR"                   -> InternalError,
+          "INCOME_SOURCE_NOT_FOUND"            -> NotFoundError,
+          "SUBMITTED_TAX_YEAR_NOT_SUPPORTED"   -> RuleTaxYearNotSupportedError,
+          "TAX_YEAR_NOT_SUPPORTED"             -> RuleTaxYearNotSupportedError,
+          "MISSING_EXPENSES"                   -> InternalError,
+          "INVALID_SUBMISSION_END_DATE"        -> RuleAdvanceSubmissionRequiresPeriodEndDateError,
+          "SUBMISSION_END_DATE_VALUE"          -> RuleSubmissionEndDateCannotMoveBackwardsError,
+          "INVALID_START_DATE"                 -> RuleStartDateNotAlignedWithReportingTypeError,
+          "START_DATE_NOT_ALIGNED"             -> RuleStartDateNotAlignedToCommencementDateError,
+          "END_DATE_NOT_ALIGNED"               -> RuleEndDateNotAlignedWithReportingTypeError,
+          "MISSING_SUBMISSION_DATES"           -> RuleMissingSubmissionDatesError,
+          "START_END_DATE_NOT_ACCEPTED"        -> RuleStartAndEndDateNotAllowedError,
+          "OUTSIDE_AMENDMENT_WINDOW"           -> RuleOutsideAmendmentWindowError,
+          "EARLY_DATA_SUBMISSION_NOT_ACCEPTED" -> RuleEarlyDataSubmissionNotAcceptedError,
+          "DUPLICATE_COUNTRY_CODE"             -> RuleDuplicateCountryCodeError,
+          "SERVER_ERROR"                       -> InternalError,
+          "SERVICE_UNAVAILABLE"                -> InternalError
+        )
 
-      val errors = List(
-        "INVALID_TAXABLE_ENTITY_ID"          -> NinoFormatError,
-        "INVALID_INCOME_SOURCE_ID"           -> BusinessIdFormatError,
-        "INVALID_PAYLOAD"                    -> InternalError,
-        "INVALID_CORRELATION_ID"             -> InternalError,
-        "INVALID_TAX_YEAR"                   -> InternalError,
-        "INCOME_SOURCE_NOT_FOUND"            -> NotFoundError,
-        "SUBMITTED_TAX_YEAR_NOT_SUPPORTED"   -> RuleTaxYearNotSupportedError,
-        "TAX_YEAR_NOT_SUPPORTED"             -> RuleTaxYearNotSupportedError,
-        "MISSING_EXPENSES"                   -> InternalError,
-        "INVALID_SUBMISSION_END_DATE"        -> RuleAdvanceSubmissionRequiresPeriodEndDateError,
-        "SUBMISSION_END_DATE_VALUE"          -> RuleSubmissionEndDateCannotMoveBackwardsError,
-        "INVALID_START_DATE"                 -> RuleStartDateNotAlignedWithReportingTypeError,
-        "START_DATE_NOT_ALIGNED"             -> RuleStartDateNotAlignedToCommencementDateError,
-        "END_DATE_NOT_ALIGNED"               -> RuleEndDateNotAlignedWithReportingTypeError,
-        "MISSING_SUBMISSION_DATES"           -> RuleMissingSubmissionDatesError,
-        "START_END_DATE_NOT_ACCEPTED"        -> RuleStartAndEndDateNotAllowedError,
-        "OUTSIDE_AMENDMENT_WINDOW"           -> RuleOutsideAmendmentWindowError,
-        "EARLY_DATA_SUBMISSION_NOT_ACCEPTED" -> RuleEarlyDataSubmissionNotAcceptedError,
-        "DUPLICATE_COUNTRY_CODE"             -> RuleDuplicateCountryCodeError,
-        "SERVER_ERROR"                       -> InternalError,
-        "SERVICE_UNAVAILABLE"                -> InternalError
-      )
-
-      errors.foreach((serviceError _).tupled)
-    }
+        errors.foreach((serviceError _).tupled)
+      }
   }
 
 }

28e9668e1285219cf8d93f22b4f1816347d77120
naglisr <18595726+naglisr@users.noreply.github.com>
2024-12-06 12:07:31 +0000

    Update ITs

--- it/v5/createAmendUkPropertyAnnualSubmission/def2/Def2_CreateAmendUkPropertyAnnualSubmissionISpec.scala
+++ it/v5/createAmendUkPropertyAnnualSubmission/def2/Def2_CreateAmendUkPropertyAnnualSubmissionISpec.scala
@@ -169,7 +169,7 @@ class Def2_CreateAmendUkPropertyAnnualSubmissionISpec extends IntegrationBaseSpe
             |      "zeroEmissionsGoodsVehicleAllowance": 2000.6567890,
             |      "businessPremisesRenovationAllowance": 2000.75678900,
             |      "otherCapitalAllowance": 2000.56789080,
-            |      "costOfReplacingDomesticGoods": 2000.95678900,
+            |      "costOfReplacingDomesticItems": 2000.95678900,
             |      "structuredBuildingAllowance": [
             |        {
             |          "amount": 3000.36780,
@@ -324,7 +324,7 @@ class Def2_CreateAmendUkPropertyAnnualSubmissionISpec extends IntegrationBaseSpe
           |      "zeroEmissionsGoodsVehicleAllowance": 2000.6235450,
           |      "businessPremisesRenovationAllowance": 2000.745360,
           |      "otherCapitalAllowance": 2000.8243520,
-          |      "costOfReplacingDomesticGoods": 2000.9064532,
+          |      "costOfReplacingDomesticItems": 2000.9064532,
           |      "structuredBuildingAllowance": [
           |        {
           |          "amount": 3000.303645,

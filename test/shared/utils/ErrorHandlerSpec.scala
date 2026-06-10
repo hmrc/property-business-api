4c81517d27db0d2e2186687fc9d2f68cf6b4c1a4
aaocs <132440545+aaocs@users.noreply.github.com>
2026-06-10 13:53:41 +0100

    added changes

--- test/shared/utils/ErrorHandlerSpec.scala
+++ test/shared/utils/ErrorHandlerSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,14 +18,13 @@ package shared.utils
 
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Configuration
-import play.api.http.Status
 import play.api.libs.json.Json
 import play.api.mvc.{AnyContent, RequestHeader, Result}
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
 import shared.models.errors.*
 import uk.gov.hmrc.auth.core.InsufficientEnrolments
-import uk.gov.hmrc.http.{HeaderCarrier, JsValidationException, NotFoundException, UpstreamErrorResponse}
+import uk.gov.hmrc.http.*
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.audit.http.connector.AuditResult.Success
 import uk.gov.hmrc.play.audit.model.{DataEvent, TruncationLog}
@@ -40,10 +39,10 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
 
   "onClientError" should {
     "return 404 with error body" when {
-      s"URI not found" in new Test {
+      "URI not found" in new Test {
 
-        val result: Future[Result] = handler.onClientError(requestHeader, Status.NOT_FOUND, "test")
-        status(result) shouldBe Status.NOT_FOUND
+        val result: Future[Result] = handler.onClientError(requestHeader, NOT_FOUND, "test")
+        status(result) shouldBe NOT_FOUND
 
         contentAsJson(result) shouldBe NotFoundError.asJson
       }
@@ -116,7 +115,6 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
     "return 401 with error body" when {
       "AuthorisationException thrown" in new Test {
         val result: Future[Result] = handler.onServerError(requestHeader, new InsufficientEnrolments("test") with NoStackTrace)
-        // TODO This really should be FORBIDDEN (403), but would need to be changed across all the APIs at once (if at all).
         status(result) shouldBe UNAUTHORIZED
 
         contentAsJson(result) shouldBe ClientOrAgentNotAuthorisedError.asJson
@@ -131,6 +129,14 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
 
         contentAsJson(result) shouldBe BadRequestError.asJson
       }
+
+      "Upstream4xxResponse thrown" in new Test {
+        val ex: UpstreamErrorResponse = UpstreamErrorResponse("client error", TOO_MANY_REQUESTS, TOO_MANY_REQUESTS, None.orNull)
+        val result: Future[Result]    = handler.onServerError(requestHeader, ex)
+
+        status(result) shouldBe BAD_REQUEST
+        contentAsJson(result) shouldBe BadRequestError.asJson
+      }
     }
 
     "return 500 with error body" when {
@@ -140,9 +146,24 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
 
         contentAsJson(result) shouldBe InternalError.asJson
       }
+
+      "Upstream5xxResponse thrown" in new Test {
+        val ex: UpstreamErrorResponse = UpstreamErrorResponse("server error", SERVICE_UNAVAILABLE, SERVICE_UNAVAILABLE, None.orNull)
+        val result: Future[Result]    = handler.onServerError(requestHeader, ex)
+
+        status(result) shouldBe INTERNAL_SERVER_ERROR
+        contentAsJson(result) shouldBe InternalError.asJson
+      }
     }
 
     "return GATEWAY_TIMEOUT with error body" when {
+      "GatewayTimeoutException is thrown" in new Test {
+        val result: Future[Result] = handler.onServerError(requestHeader, new GatewayTimeoutException("test") with NoStackTrace)
+
+        status(result) shouldBe GATEWAY_TIMEOUT
+        contentAsJson(result) shouldBe GatewayTimeoutError.asJson
+      }
+
       Seq(499, GATEWAY_TIMEOUT).foreach { statusCode =>
         s"a $statusCode UpstreamErrorResponse is returned" in new Test {
           val errorResponse: UpstreamErrorResponse = UpstreamErrorResponse("request timeout", statusCode, statusCode, Map.empty)
@@ -155,19 +176,19 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
     }
   }
 
-  def anyVersionHeader: (String, String) = ACCEPT -> s"application/vnd.hmrc.1.0+json"
+  def anyVersionHeader: (String, String) = ACCEPT -> "application/vnd.hmrc.1.0+json"
 
-  class Test {
+  private trait Test {
     val method = "some-method"
 
     val requestHeader: FakeRequest[AnyContent] = FakeRequest().withHeaders(anyVersionHeader)
 
-    val auditConnector: AuditConnector = mock[AuditConnector]
-    val httpAuditEvent: HttpAuditEvent = mock[HttpAuditEvent]
+    private val auditConnector: AuditConnector = mock[AuditConnector]
+    private val httpAuditEvent: HttpAuditEvent = mock[HttpAuditEvent]
 
-    val eventTags: Map[String, String] = Map("transactionName" -> "event.transactionName")
+    private val eventTags: Map[String, String] = Map("transactionName" -> "event.transactionName")
 
-    val dataEvent: DataEvent = DataEvent(
+    private val dataEvent: DataEvent = DataEvent(
       auditSource = "auditSource",
       auditType = "event.auditType",
       eventId = "",
@@ -186,7 +207,7 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
       .expects(*, *, *)
       .returns(Future.successful(Success))
 
-    val configuration: Configuration = Configuration(
+    private val configuration: Configuration = Configuration(
       "appName"                                         -> "myApp",
       "bootstrap.errorHandler.warnOnly.statusCodes"     -> List(OK),
       "bootstrap.errorHandler.suppress4xxErrorMessages" -> false,

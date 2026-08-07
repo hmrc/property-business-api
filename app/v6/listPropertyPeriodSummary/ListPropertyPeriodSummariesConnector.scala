5cac9ea8f7aa7823c3d1cf45b45d78bbbd45dc95
Ben Ryan <37295698+benryan1790@users.noreply.github.com>
2026-08-07 15:48:40 +0100

    MTDSA-35971: 1954 hip migration (#663)
    * MTDSA-35971: rename packages
    
    * MTDSA-35971: 1954 hip migration

---
+++ app/v6/listPropertyPeriodSummary/ListPropertyPeriodSummariesConnector.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v6.listPropertyPeriodSummary
+
+import api.config.{AppConfig, ConfigFeatureSwitches}
+import api.connectors.DownstreamUri.{HipUri, IfsUri}
+import api.connectors.httpparsers.StandardDownstreamHttpParser.reads
+import api.connectors.{BaseDownstreamConnector, DownstreamOutcome}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.client.HttpClientV2
+import v6.listPropertyPeriodSummary.model.request.ListPropertyPeriodSummariesRequestData
+import v6.listPropertyPeriodSummary.model.response.ListPropertyPeriodSummariesResponse
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class ListPropertyPeriodSummariesConnector @Inject() (val http: HttpClientV2, val appConfig: AppConfig) extends BaseDownstreamConnector {
+
+  def listPeriodSummaries(
+      request: ListPropertyPeriodSummariesRequestData
+  )(implicit
+      hc: HeaderCarrier,
+      ec: ExecutionContext,
+      correlationId: String
+  ): Future[DownstreamOutcome[ListPropertyPeriodSummariesResponse]] = {
+
+    import request.*
+
+    val (downstreamUri, queryParams) = if (taxYear.useTaxYearSpecificApi) {
+      if (ConfigFeatureSwitches().isEnabled("ifs_hip_migration_1954")) {
+        (
+          HipUri[ListPropertyPeriodSummariesResponse](
+            s"itsa/income-tax/v1/${taxYear.asTysDownstream}/business/property/periodic/$nino/$businessId/submissions"),
+          Nil
+        )
+      } else {
+        (
+          IfsUri[ListPropertyPeriodSummariesResponse](s"income-tax/business/property/${taxYear.asTysDownstream}/$nino/$businessId/period"),
+          Nil
+        )
+      }
+    } else {
+      (
+        IfsUri[ListPropertyPeriodSummariesResponse](s"income-tax/business/property/$nino/$businessId/period"),
+        // Note that MTD tax year format is used
+        List("taxYear" -> taxYear.asMtd)
+      )
+    }
+
+    get(downstreamUri, queryParams)
+  }
+
+}
---
+++ test/v6/listPropertyPeriodSummary/ListPropertyPeriodSummariesControllerSpec.scala
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v6.listPropertyPeriodSummary
+
+import api.controllers.{ControllerBaseSpec, ControllerTestRunner}
+import api.models.domain.{BusinessId, Nino, TaxYear}
+import api.models.errors.*
+import api.models.outcomes.ResponseWrapper
+import play.api.Configuration
+import play.api.libs.json.Json
+import play.api.mvc.Result
+import v6.listPropertyPeriodSummary.def1.model.response.SubmissionPeriod
+import v6.listPropertyPeriodSummary.model.request.ListPropertyPeriodSummariesRequestData
+import v6.listPropertyPeriodSummary.model.response.ListPropertyPeriodSummariesResponse
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class ListPropertyPeriodSummariesControllerSpec
+    extends ControllerBaseSpec
+    with ControllerTestRunner
+    with MockListPropertyPeriodSummariesService
+    with MockListPropertyPeriodSummariesValidatorFactory {
+
+  private val businessId = "XAIS12345678910"
+  private val taxYear    = "2020-21"
+
+  "ListPropertyPeriodSummariesController" should {
+    "return Ok" when {
+      "the request received is valid" in new Test {
+        willUseValidator(returningSuccess(requestData))
+
+        MockListPropertyPeriodSummariesService
+          .listPeriodSummaries(requestData)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, responseData))))
+
+        runOkTest(expectedStatus = OK, maybeExpectedResponseBody = Some(Json.toJson(responseData)))
+      }
+    }
+
+    "return an error as per spec" when {
+      "the parser validation fails" in new Test {
+        willUseValidator(returning(NinoFormatError))
+
+        runErrorTest(NinoFormatError)
+      }
+
+      "the service returns an error" in new Test {
+        willUseValidator(returningSuccess(requestData))
+
+        MockListPropertyPeriodSummariesService
+          .listPeriodSummaries(requestData)
+          .returns(Future.successful(Left(ErrorWrapper(correlationId, RuleTaxYearNotSupportedError))))
+
+        runErrorTest(RuleTaxYearNotSupportedError)
+
+      }
+    }
+  }
+
+  trait Test extends ControllerTest {
+
+    protected val controller: ListPropertyPeriodSummariesController = new ListPropertyPeriodSummariesController(
+      authService = mockEnrolmentsAuthService,
+      lookupService = mockMtdIdLookupService,
+      validatorFactory = mockListPropertyPeriodSummariesValidatorFactory,
+      service = mockService,
+      cc = cc,
+      idGenerator = mockIdGenerator
+    )
+
+    MockedAppConfig.featureSwitchConfig.anyNumberOfTimes() returns Configuration(
+      "supporting-agents-access-control.enabled" -> true
+    )
+
+    MockedAppConfig.endpointAllowsSupportingAgents(controller.endpointName).anyNumberOfTimes() returns false
+
+    protected def callController(): Future[Result] = controller.handleRequest(validNino, businessId, taxYear)(fakeGetRequest)
+
+    protected val requestData: ListPropertyPeriodSummariesRequestData =
+      ListPropertyPeriodSummariesRequestData(Nino(validNino), BusinessId(businessId), TaxYear.fromMtd(taxYear))
+
+    protected val responseData: ListPropertyPeriodSummariesResponse =
+      ListPropertyPeriodSummariesResponse(List(SubmissionPeriod("someId", "fromDate", "toDate")))
+
+  }
+
+}
